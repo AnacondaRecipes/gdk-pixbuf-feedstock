4a770ce35fc0680b90ed78cb6acd6222515bdf10
Kai Tietz <ktietz@anaconda.com>
2021-09-16 13:41:46 +0200

    adjust for windows to be built

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -19,12 +19,12 @@ set ^"MESON_OPTIONS=^
   -D x11=false ^
  ^"
 
-:: configure build using meson
-%BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\meson setup builddir !MESON_OPTIONS!
+meson setup builddir !MESON_OPTIONS!
+
 if errorlevel 1 exit 1
 
 :: print results of build configuration
-%BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\meson configure builddir
+meson configure builddir
 if errorlevel 1 exit 1
 
 ninja -v -C builddir -j %CPU_COUNT%
